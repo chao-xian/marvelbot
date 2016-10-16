f6d99f343c6ed8c1b3a6158d3a6e467467908df7
chao-xian <dev@kelvingan.co.uk>
2016-10-16 22:30:06 +0100

    Move Marvel API creds to env vars and add spiderman method

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,5 +1,6 @@
 class Bot < SlackRubyBot::Bot
   @id = 0
+  @marvel_client = Marvelite::API::Client.new( public_key: ENV['MARVEL_PUBLIC_KEY'], private_key: ENV['MARVEL_PRIVATE_KEY'])
 
   def self.next_id
     @id = @id % 10 + 1
@@ -8,5 +9,11 @@ class Bot < SlackRubyBot::Bot
   command 'say' do |client, data, match|
     Rails.cache.write next_id, { text: match['expression'] }
     client.say(channel: data.channel, text: match['expression'])
+  end
+
+  command 'spiderman' do |client, data, match|
+    spidey_results = @marvel_client.character('Spider-Man')
+    # client.say(text: "#{spidey_results[:results][:thumbnail][:path]}.#{spidey_results[:results][:thumbnail]}", channel: data.channel)
+    client.say(text: "#{spidey_results[:data][:results][0][:thumbnail][:path]}.#{spidey_results[:data][:results][0][:thumbnail][:extension]}", channel: data.channel)
   end
 end
